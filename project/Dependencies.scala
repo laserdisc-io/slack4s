0548cb5ce042b1070af290ce1f85e42edcb8762e
Scala Steward <me@scala-steward.org>
2022-05-06 09:23:43 +0200

    Update log4cats-slf4j to 2.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
 
   val Logging = Seq(
     libraryDependencies ++= Seq(
-      "org.typelevel" %% "log4cats-slf4j"  % "2.2.0",
+      "org.typelevel" %% "log4cats-slf4j"  % "2.3.1",
       "ch.qos.logback" % "logback-classic" % "1.2.10" % Test,
       "ch.qos.logback" % "logback-core"    % "1.2.10" % Test,
       "org.slf4j"      % "jcl-over-slf4j"  % "1.7.35" % Test,
