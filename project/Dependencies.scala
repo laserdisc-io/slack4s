212e7fc2d4ff82227db85b1367730f720623c9d6
Scala Steward <scala_steward@virtuslab.com>
2023-04-13 16:25:27 +0000

    Update slack-app-backend to 1.29.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   )
 
   val Slack = Seq(
-    libraryDependencies += "com.slack.api" % "slack-app-backend" % "1.27.3"
+    libraryDependencies += "com.slack.api" % "slack-app-backend" % "1.29.2"
   )
 
   val Refined = Seq(
