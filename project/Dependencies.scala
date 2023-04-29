e45e9142c1fd6915e941c617ae748da8be80657d
Scala Steward <scala_steward@virtuslab.com>
2023-04-29 16:11:40 +0000

    Update mockito-core to 5.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     testFrameworks += new TestFramework("munit.Framework"),
     libraryDependencies ++= Seq(
       "org.scalameta" %% "munit"         % "0.7.29",
-      "org.mockito"    % "mockito-core"  % "5.3.0",
+      "org.mockito"    % "mockito-core"  % "5.3.1",
       "org.gnieh"     %% "diffson-circe" % "4.4.0"
     ).map(_ % Test)
   )
