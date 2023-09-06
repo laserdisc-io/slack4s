5e83ed6e529da4db276bee268ef036ddc717bfe8
Scala Steward <scala_steward@virtuslab.com>
2023-09-06 23:47:58 +0000

    Update circe-core, circe-parser to 0.14.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Dependencies {
     )
   )
 
-  val CirceVersion = "0.14.5"
+  val CirceVersion = "0.14.6"
   val Circe = Seq(
     libraryDependencies ++= Seq(
       "io.circe" %% "circe-core"   % CirceVersion,
