e143144781bf24992eb02b81cc1ddff43010e715
Scala Steward <scala_steward@virtuslab.com>
2025-04-30 16:38:26 +0000

    Update munit to 1.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val TestLib = Seq(
     testFrameworks += new TestFramework("munit.Framework"),
     libraryDependencies ++= Seq(
-      "org.scalameta" %% "munit"         % "1.1.0",
+      "org.scalameta" %% "munit"         % "1.1.1",
       "org.mockito"    % "mockito-core"  % "5.16.0",
       "org.gnieh"     %% "diffson-circe" % "4.6.0"
     ).map(_ % Test)
