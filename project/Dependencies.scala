20eba6ad2ad5317a8bcd831e28f2a29d3f2f219e
Scala Steward <scala_steward@virtuslab.com>
2026-04-13 17:51:19 +0000

    Update http4s-circe, http4s-dsl, ... to 0.23.34

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
     "http4s-ember-server",
     "http4s-ember-client",
     "http4s-circe"
-  ).map("org.http4s" %% _ % "0.23.33")
+  ).map("org.http4s" %% _ % "0.23.34")
 
   val CirceVersion = "0.14.15"
   val Circe        = libraryDependencies ++= Seq(
